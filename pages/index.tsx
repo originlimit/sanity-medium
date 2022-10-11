7bf76d5b1507a40bd948acc44189e7da1ecc5739
originlimit <andres-log@protonmail.com>
2022-10-11 13:05:03 -0700

    changed title

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className='flex'>
