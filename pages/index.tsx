842a04f35f02b46e5dfbb5711b7dab84820969e9
originlimit <andres-log@protonmail.com>
2022-10-11 13:11:30 -0700

    imported and testing header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
@@ -8,9 +9,7 @@ const Home: NextPage = () => {
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='flex'>
-        <h1 className='text-blue-500'>this is a test</h1>
-      </main>
+      <Header />
     </div>
   )
 }
