41dd37d703a19ca3425cbb4ce42faf3a4f0961a2
originlimit <andres-log@protonmail.com>
2022-10-12 13:00:39 -0700

    styled header and fixed positioning

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>This is the Header</div>
+    <header className='fixed flex justify-center w-screen z-99 top-0 bg-blue-100 backdrop-blur-md p-4'>
+      <div className='flex w-full justify-between text-lg'>
+        <span className='font-bold'>MEDIUM</span>
+        <span>Home Services Pricing About</span>
+      </div>
+    </header>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div>
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
