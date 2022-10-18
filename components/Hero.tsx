3f1c39487f82ac55c433512831256975ae6c201b
originlimit <andres-log@protonmail.com>
2022-10-18 00:55:47 -0700

    testing out hero layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>This is a hero component</div>
+    <div className='flex justify-center align-middle bg-red-500 h-96'>
+      <section>
+        <h1>Medium title</h1>
+        <p>This is the hero paragraph body</p>
+      </section>
+      <section>this is a picture</section>
+    </div>
   )
 }
 
