18cf0b20d434a264266550a75c0c4a067f60f8a3
originlimit <andres-log@protonmail.com>
2022-10-18 19:51:12 -0700

    created svg reference and added it to Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import Image from 'next/image'
+import blogImg from '../assets/undraw_fashion_blogging_re_fhi5.svg'
 
 const Hero = () => {
   return (
     <div className='flex justify-center align-middle bg-red-500 h-96'>
       <section>
-        <h1>Medium title</h1>
-        <p>This is the hero paragraph body</p>
+        <h1>Discover greatness.</h1>
+        <p>Read the thoughts and stories of experts and talented writers in various topics.</p>
       </section>
-      <section>this is a picture</section>
+      <Image src={blogImg} alt='blogging' />
     </div>
   )
 }
