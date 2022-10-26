13b0a78ba3d8366786d56d1e5ef8d495c0d01a9a
originlimit <andres-log@protonmail.com>
2022-10-25 21:14:46 -0700

    added button, finished centering, modified SVG

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import Image from 'next/image'
-import blogImg from '../assets/undraw_fashion_blogging_re_fhi5.svg'
+import blogImg from '../assets/heroimg.svg'
 
 const Hero = () => {
   return (
     <div className='flex justify-center bg-yellow-500'>
-      <div className='flex justify-center items-center h-96 max-w-7xl px-10 py-5'>
-        <section className='p-0 flex-col items-center justify-center w-1/2'>
-          <h1 className='my-3 font-serif text-left text-5xl font-semibold'><span className='underline font-serif'>Medium:</span> discover greatness.</h1>
-          <p className='my-3 text-2xl text-left'>Read the thoughts and stories of experts and talented writers in various topics.</p>
+      <div className='flex justify-center items-center h-96 max-w-7xl px-10 py-3'>
+        <section className='p-2 flex-col items-center w-1/2 justify-center align-middle'>
+          <h1 className='py-3 font-serif text-left text-5xl font-semibold'><span className='underline font-serif'>Medium:</span> discover greatness.</h1>
+          <p className='py-3 text-2xl text-left'>Read the thoughts and stories of experts and talented writers in various topics.</p>
+          <h3 className='bg-black hover:bg-gray-500 text-white my-5 text-xl max-w-xs py-2 px-4 rounded-full cursor-pointer'>Start reading</h3>
         </section>
-        <div className='flex justify-center px-5 w-1/2 max-h-80'>
+        <div className='flex justify-center w-1/2'>
           <Image src={blogImg} alt='blogging' />
         </div>
       </div>
